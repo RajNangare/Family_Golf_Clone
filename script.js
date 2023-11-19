7306e6ac0548a5c88ba98fa38acaf69fd6078fb2
RajNangare <>
2023-11-20 03:34:22 +0530

    updating chanegs

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ document.addEventListener("mousemove", function(dets){
     crs.style.left = dets.x - 7.5 + "px" 
     crs.style.top = dets.y - 7.5 + "px" 
 })
-var crm = document.querySelector("#bigcursor")
-document.addEventListener("mousemove", function(dets){
-    crm.style.left = dets.x - 250 + "px" 
-    crm.style.top = dets.y - 250 + "px" 
-})
+// var crm = document.querySelector("#bigcursor")
+// document.addEventListener("mousemove", function(dets){
+//     crm.style.left = dets.x - 250 + "px" 
+//     crm.style.top = dets.y - 250 + "px" 
+// })
 
 gsap.to("#main", {
     backgroundColor: "#000",
@@ -28,8 +28,8 @@ gsap.to("#main", {
         trigger : "#main",
         scroller : "body",
         // markers : true,
-        start : "30%",
-        end : "50%",
+        start : "10%",
+        end : "30%",
         scrub : 2
     }
 })
