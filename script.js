794a6b83a1c8731a25d2a40b30dd90400716b73f
RajNangare <>
2023-11-20 10:33:11 +0530

    bug fixed
    bug fixed slider hover

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ document.addEventListener("mousemove", function(dets){
     crs.style.left = dets.x - 7.5 + "px" 
     crs.style.top = dets.y - 7.5 + "px" 
 })
-// var crm = document.querySelector("#bigcursor")
-// document.addEventListener("mousemove", function(dets){
-//     crm.style.left = dets.x - 250 + "px" 
-//     crm.style.top = dets.y - 250 + "px" 
-// })
+var crm = document.querySelector("#bigcursor")
+document.addEventListener("mousemove", function(dets){
+    crm.style.left = dets.x - 250 + "px" 
+    crm.style.top = dets.y - 250 + "px" 
+})
 
 gsap.to("#main", {
     backgroundColor: "#000",
